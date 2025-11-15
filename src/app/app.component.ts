f78f520e734cc8e5a05e968f2d1bc8840993fb2d
Deshev1 <stefandeshev@gmail.com>
2025-11-15 19:40:19 +0200

    Added component type to app component name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { Home } from './components/home/home.component';
 @Component({
   selector: 'app-root',
   imports: [Home],
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class App {
   protected title = 'Chatterbox-Angular';
